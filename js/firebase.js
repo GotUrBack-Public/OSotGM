7f0577f94b9b6169e4bea59e1f0743f02aa737ac
Anonym Arch User <75336897532579754357a@gmail.com>
2026-09-20 09:57:09 +0200

    Initialize Firebase app and export services

---
+++ js/firebase.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-storage.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAiCDS9EHVWQ041trH8kgwxHZUYJnqjCB8",
+  authDomain: "osotgm.firebaseapp.com",
+  projectId: "osotgm",
+  storageBucket: "osotgm.firebasestorage.app",
+  messagingSenderId: "723502696230",
+  appId: "1:723502696230:web:90bc11be79961a1d3ae3e7",
+  measurementId: "G-CXWEC1D7GE"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+
+export { app };
+
+
