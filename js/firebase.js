e63d5b88a976033cdb5f2f538555395c57230042
Anonym Arch User <75336897532579754357a@gmail.com>
2026-09-20 12:50:55 +0200

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -2,79 +2,26 @@
 // FIREBASE KONFIGURATION
 // ==================================================
 
-import {
-  initializeApp
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
-import {
-  getAuth
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
-import {
-  getFirestore
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-
-// ==================================================
-// FIREBASE CONFIG
-// ==================================================
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-
-  apiKey:
-    "AIzaSyAiCDS9EHVWQ041trH8kgwxHZUYJnqjCB8",
-
-  authDomain:
-    "osotgm.firebaseapp.com",
-
-  projectId:
-    "osotgm",
-
-  storageBucket:
-    "osotgm.firebasestorage.app",
-
-  messagingSenderId:
-    "723502696230",
-
-  appId:
-    "1:723502696230:web:90bc11be79961a1d3ae3e7",
-
-  measurementId:
-    "G-CXWEC1D7GE"
-
+  apiKey: "AIzaSyAiCDS9EHVWQ041trH8kgwxHZUYJnqjCB8",
+  authDomain: "osotgm.firebaseapp.com",
+  projectId: "osotgm",
+  storageBucket: "osotgm.firebasestorage.app",
+  messagingSenderId: "723502696230",
+  appId: "1:723502696230:web:90bc11be79961a1d3ae3e7",
+  measurementId: "G-CXWEC1D7GE"
 };
 
+const app = initializeApp(firebaseConfig);
 
-// ==================================================
-// FIREBASE INITIALISIEREN
-// ==================================================
+const auth = getAuth(app);
+const db = getFirestore(app);
 
-const app =
-  initializeApp(firebaseConfig);
-
-
-// ==================================================
-// AUTH
-// ==================================================
-
-const auth =
-  getAuth(app);
+export { app, auth, db };
 
 
-// ==================================================
-// FIRESTORE
-// ==================================================
 
-const db =
-  getFirestore(app);
-
-
-// ==================================================
-// EXPORTS
-// ==================================================
-
-export {
-  app,
-  auth,
-  db
-};
